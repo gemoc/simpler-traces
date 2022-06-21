d7ec372ac100429657658ce1718dd76038ea04ed
Erwan Bousse <erwan.bousse@ls2n.fr>
2022-06-21 13:39:20 +0200

    Remove xtend-gen

--- org.eclipse.gemoc.trace.simple.addon/xtend-gen/org/eclipse/gemoc/trace/simple/addon/Activator.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.gemoc.trace.simple.addon;
-
-@SuppressWarnings("all")
-public class Activator {
-  public static void error(final String string, final UnsupportedOperationException exception) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-}
